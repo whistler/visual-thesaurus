2eedc5d68fae09021f34f7aab221a619bd88d350
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2013-01-04 23:18:18 -0800

    created a welcome controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theviz::Application.config.secret_token = '4baedb5c8c4862a56e7916a8a26d950a96df2d74a2ebcebb182f0ef6818b28daa22f9105cf1410ac1e65591fd3e2080c12b42edbcaf7a102186c7ec0d5cea96f'
